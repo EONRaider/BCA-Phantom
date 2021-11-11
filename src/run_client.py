97f4521a97ddc9050dca22d00bdeca8ca960138f
EONRaider <livewire_voodoo@protonmail.com>
2021-11-11 18:51:00 -0300

    Conform to new interface of Client class

--- src/run_client.py
+++ src/run_client.py
@@ -9,16 +9,19 @@ from the shell.cfg file found in this same directory."""
 from configparser import ConfigParser
 from pathlib import Path
 
-from src.client import Client
+from client import Client
 
 
-shell_cfg = Path(__file__).parent.joinpath("shell.cfg")
-(cfg := ConfigParser()).read(shell_cfg)
-server_cfg = cfg["SERVER"]
-certs_cfg = cfg["CERTIFICATES"]
+def client():
+    shell_cfg = Path(__file__).parent.joinpath("shell.cfg")
+    (cfg := ConfigParser()).read(shell_cfg)
+    server_cfg = cfg["SERVER"]
 
-Client(
-    server_address=server_cfg.get("hostname"),
-    server_port=server_cfg.getint("port"),
-    ca_file=certs_cfg.get("ca-cert")
-).execute()
+    Client(
+        server_address=server_cfg.get("hostname"),
+        server_port=server_cfg.getint("port")
+    ).execute()
+
+
+if __name__ == "__main__":
+    client()
