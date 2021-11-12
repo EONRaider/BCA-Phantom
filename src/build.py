c53f9f9bea34355d96b04e2e92346b470c3cc8b7
EONRaider <livewire_voodoo@protonmail.com>
2021-11-12 18:30:24 -0300

    Refactor build module
    New functions and CLI parser were implemented

--- src/build.py
+++ src/build.py
@@ -1,30 +1,77 @@
+#!/usr/bin/env python3
+# https://github.com/EONRaider/BCA-HTTPS-Reverse-Shell
+
+__author__ = "EONRaider @ keybase.io/eonraider"
+
+import configparser
+
 import PyInstaller.__main__ as pyinstaller
 
 
-def build(mode: str):
-    commands = {
-        "server": "run_server.py "
-                  "--onefile "
-                  "--add-data shell.cfg:. "
-                  "--add-data ca.pem:. "
-                  "--add-data server.pem:.",
-        "client": "run_client.py "
-                  "--onefile "
-                  "--add-data shell.cfg:. "
-                  "--add-data ca.pem:."
+def server(args):
+    cmd = ["server.py", "--onefile"]
+    if args.server_cert is not None:
+        cmd.extend(["--add-data", f"{args.server_cert}:."])
+    pyinstaller.run(cmd)
+
+
+def client(args):
+    config = configparser.ConfigParser()
+    config["CLIENT"] = {
+        "host": args.host,
+        "port": str(args.port),
+        "ca-certificate": args.ca_cert
     }
-    pyinstaller.run(commands[mode].split(" "))
+
+    with open(file="client.cfg", mode="w") as config_file:
+        config.write(config_file)
+
+    cmd = ["client.py",
+           "--onefile",
+           "--add-data", f"{args.ca_cert}:.",
+           "--add-data", "client.cfg:."]
+    pyinstaller.run(cmd)
 
 
 if __name__ == "__main__":
     import argparse
 
-    modes = ["server", "client"]
     parser = argparse.ArgumentParser()
-    parser.add_argument("mode",
-                        choices=modes,
-                        help="Choose between server and client modes.")
+    subparsers = parser.add_subparsers()
+
+    server_parser = subparsers.add_parser("server")
+    server_parser.add_argument(
+        "--server-cert",
+        type=str,
+        metavar="<path>",
+        help="Path to a file containing the server certificate in PEM format. "
+             "This certificate will be packaged with the compiled file for "
+             "deployment."
+    )
+    server_parser.set_defaults(func=server)
+
+    client_parser = subparsers.add_parser("client")
+    client_parser.add_argument(
+        "host",
+        type=str,
+        metavar="<hostname/address>",
+        help="Address or hostname of the server to connect to."
+    )
+    client_parser.add_argument(
+        "port",
+        type=int,
+        metavar="<port>",
+        help="Port number exposed by the server."
+    )
+    client_parser.add_argument(
+        "--ca-cert",
+        type=str,
+        metavar="<path>",
+        help="Path to a file containing the certificate for the Certificate "
+             "Authority (CA) in PEM format. This certificate will be packaged "
+             "with the compiled file for deployment."
+    )
+    client_parser.set_defaults(func=client)
 
     _args = parser.parse_args()
-
-    build(mode=_args.mode)
+    _args.func(_args)
